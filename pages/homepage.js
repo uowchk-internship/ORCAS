b48a8a1faee4efe0f8178e28ee8c2db9440d7253
IanFong062 <106955687+IanFong062@users.noreply.github.com>
2022-07-06 17:20:57 +0800

    header

---
+++ pages/homepage.js
@@ -0,0 +1,12 @@
+import Header from "../src/components/header"
+import Footer from "../src/components/footer"
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <h1>Homepage</h1>
+      <Footer />
+    </div>
+  )
+}
