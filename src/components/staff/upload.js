7dbb098fa41b3be3259e3f5d613f890cc7bd31ca
unclechuen <johnny@johnnyip.com>
2022-07-26 19:51:05 +0800

    Update upload.js

--- src/components/staff/upload.js
+++ src/components/staff/upload.js
@@ -50,6 +50,10 @@ export default function UploadComponent() {
   const [url, setUrl] = useState("https://")
   const [abstract, setAbstract] = useState("")
 
+  const labelStyle = {
+    fontSize: 20,
+  };
+
   const submitForm = async () => {
     setSubmitLoading(true)
 
@@ -131,7 +135,7 @@ export default function UploadComponent() {
     <>
 
       <div>
-        <label htmlFor="programPlanUpload">
+        <label style={labelStyle} htmlFor="programPlanUpload">
           <Button style={{ backgroundColor: "#001641", color: "#ffffff", borderRadius: 5 }}
             onClick={() => document.getElementById('programPlanUpload').click()}
             position="center" size="md"
@@ -171,19 +175,19 @@ export default function UploadComponent() {
 
       <form>
         <br />
-        <label htmlFor="topic">Topic: <span style={{ color: "#ED0A00" }}>*</span></label>
+        <label style={labelStyle} htmlFor="topic">Topic: <span style={{ color: "#ED0A00" }}>*</span></label>
         <input type="text" id="topic"
           value={topic}
           onChange={(e) => setTopic(e.target.value)}
         />
 
-        <label htmlFor="author">Author:</label>
+        <label style={labelStyle} htmlFor="author">Author:</label>
         <input type="text" id="author"
           value={author}
           onChange={(e) => setAuthor(e.target.value)}
         />
 
-        <label htmlFor="datepicker">Publish year: <span style={{ color: "#ED0A00" }}>*</span></label>
+        <label style={labelStyle} htmlFor="datepicker">Publish year: <span style={{ color: "#ED0A00" }}>*</span></label>
 
         <div style={{ width: "50%" }}>
           <Datetime
@@ -195,13 +199,13 @@ export default function UploadComponent() {
           />
         </div><br />
 
-        <label htmlFor="publisher">Publisher:</label>
+        <label style={labelStyle} htmlFor="publisher">Publisher:</label>
         <input type="text" id="publisher"
           value={publisher}
           onChange={(e) => setPublisher(e.target.value)}
         />
 
-        <label >Subject: (you can choose more than one) <span style={{ color: "#ED0A00" }}>*</span></label>
+        <label style={labelStyle} >Subject: (you can choose more than one) <span style={{ color: "#ED0A00" }}>*</span></label>
 
         <Chips value={subjects} onChange={setSubjects} multiple size="md" radius="sm" classNames={classes}>
           <Chip value="Science & Technology">Science & Technology</Chip>
@@ -212,7 +216,7 @@ export default function UploadComponent() {
         </Chips>
         <br />
 
-        <label >Resources Type: (choose one) <span style={{ color: "#ED0A00" }}>*</span></label>
+        <label style={labelStyle} >Resources Type: (choose one) <span style={{ color: "#ED0A00" }}>*</span></label>
         <Chips value={types} onChange={setTypes} size="md" radius="sm" classNames={classes}>
           <Chip value="Journal Article">Journal Article</Chip>
           <Chip value="Newspaper Article">Newspaper Article</Chip>
@@ -220,7 +224,7 @@ export default function UploadComponent() {
         </Chips>
         <br />
 
-        <label htmlFor="link">
+        <label style={labelStyle} htmlFor="link">
           Link: <span style={{ color: "#ED0A00" }}>*</span><br />
           Your link must include "https://" or "http://" at the beginning.
         </label>
@@ -230,7 +234,7 @@ export default function UploadComponent() {
         />
 
 
-        <label htmlFor="abstract">
+        <label style={labelStyle} htmlFor="abstract">
           Abstract: <span style={{ color: "#ED0A00" }}>*</span><br />
         </label>
         <textarea id="abstract" rows="4"
