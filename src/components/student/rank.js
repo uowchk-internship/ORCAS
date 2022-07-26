e010055588cd28bd553dfe7bbfa140252c3b344b
Johnny IP <johnny@johnnyip.com>
2022-07-26 15:43:06 +0800

    color bug fix

--- src/components/student/rank.js
+++ src/components/student/rank.js
@@ -1,67 +1,76 @@
-import { useState, useEffect } from 'react'
-import { Select } from '@mantine/core';
+import { useState, useEffect } from "react";
+import { Select, Modal } from "@mantine/core";
 
-import RankItem from './rankItem'
-import { getMonths } from '../../functions/ranking'
+import RankItem from "./rankItem";
+import { getMonths } from "../../functions/ranking";
 
 export default function RankComponent() {
-  const [fetched, setFetched] = useState(false)
+  const [fetched, setFetched] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
 
   //Ranking data
-  const [allRankings, setAllRankings] = useState({})
+  const [allRankings, setAllRankings] = useState({});
 
-  const [selectedMonth, setSelectedMonth] = useState("")
+  const [selectedMonth, setSelectedMonth] = useState("");
   const [months, setMonths] = useState([
-    { value: '05-2022', label: '05-2022' }
-  ])
+    { value: "05-2022", label: "05-2022" },
+  ]);
 
   useEffect(() => {
     const fetchMonths = async () => {
       let result = await getMonths();
-      console.log(result)
+      console.log(result);
 
-      let completeRankings = {}
+      let completeRankings = {};
       let completeMonths = [];
       for (let rankOfMonth of result) {
         if (rankOfMonth.length > 0) {
           //save the month name
-          let currentMonthName = rankOfMonth[rankOfMonth.length - 1].month
-          if (currentMonthName !== "overall") { completeMonths.push(currentMonthName) }
+          let currentMonthName = rankOfMonth[rankOfMonth.length - 1].month;
+          if (currentMonthName !== "overall") {
+            completeMonths.push(currentMonthName);
+          }
 
           //get fitst 5 elements
-          let firstFiveRank = []
+          let firstFiveRank = [];
           for (const [i, value] of rankOfMonth.entries()) {
             if (i < 5) {
-              firstFiveRank.push(value)
+              firstFiveRank.push(value);
             }
           }
           //save object
-          completeRankings[currentMonthName] = firstFiveRank
+          completeRankings[currentMonthName] = firstFiveRank;
         }
       }
 
       //sort with the month name
-      completeMonths.sort()
+      completeMonths.sort();
 
       // //Put into select list
-      setSelectedMonth(completeMonths[completeMonths.length - 1])
-      setMonths(completeMonths)
-      setAllRankings(completeRankings)
-      setFetched(true)
-    }
+      setSelectedMonth(completeMonths[completeMonths.length - 1]);
+      setMonths(completeMonths);
+      setAllRankings(completeRankings);
+      setFetched(true);
+    };
 
     if (!fetched) {
-      fetchMonths()
+      fetchMonths();
     }
-  })
+  });
 
   return (
     <>
       <div style={{ textAlign: "center" }}>
-        <div className="uw-blockhead" style={{ width: "50%", display: "inline-block" }}>
+        <div
+          className="uw-blockhead"
+          style={{ width: "50%", display: "inline-block" }}
+        >
           Monthly Ranking
         </div>
-        <h2 className="uw-blockhead" style={{ width: "50%", display: "inline-block" }}>
+        <h2
+          className="uw-blockhead"
+          style={{ width: "50%", display: "inline-block" }}
+        >
           Overall Ranking
         </h2>
       </div>
@@ -73,10 +82,17 @@ export default function RankComponent() {
           onChange={setSelectedMonth}
           data={months}
         />
-
       </div>
 
-      <div style={{ display: "inline-block", width: "50%", paddingLeft: 20, paddingRight: 20, verticalAlign: "top" }} >
+      <div
+        style={{
+          display: "inline-block",
+          width: "50%",
+          paddingLeft: 20,
+          paddingRight: 20,
+          verticalAlign: "top",
+        }}
+      >
         <table>
           <thead>
             <tr>
@@ -86,16 +102,26 @@ export default function RankComponent() {
             </tr>
           </thead>
           <tbody>
-            {(allRankings[selectedMonth] !== undefined) ?
+            {allRankings[selectedMonth] !== undefined ? (
               [...allRankings[selectedMonth]].map((item, i) => {
-                return <RankItem data={item} key={i} sequence={i} />
-              }) : <></>}
-
+                return <RankItem data={item} key={i} sequence={i} />;
+              })
+            ) : (
+              <></>
+            )}
           </tbody>
         </table>
       </div>
 
-      <div style={{ display: "inline-block", width: "50%", paddingLeft: 20, paddingRight: 20, verticalAlign: "top" }} >
+      <div
+        style={{
+          display: "inline-block",
+          width: "50%",
+          paddingLeft: 20,
+          paddingRight: 20,
+          verticalAlign: "top",
+        }}
+      >
         <table>
           <thead>
             <tr>
@@ -105,21 +131,31 @@ export default function RankComponent() {
             </tr>
           </thead>
           <tbody>
-            {(allRankings["overall"] !== undefined) ?
+            {allRankings["overall"] !== undefined ? (
               [...allRankings["overall"]].map((item, i) => {
-                return <RankItem data={item} key={i} sequence={i} />
-              }) : <></>}
+                return <RankItem data={item} key={i} sequence={i} />;
+              })
+            ) : (
+              <></>
+            )}
           </tbody>
         </table>
       </div>
-
 
       <div className="popup">
-        Click me to check the rewards of top 3 students
+        <a onClick={() => {setShowPopup(true)}}>Click me</a> to check the rewards of top 3 students
         <span className="popuptext" id="myPopup">
           you have got the rewards! congrad!
         </span>
       </div>
+
+      <Modal
+        opened={showPopup}
+        onClose={() => setShowPopup(false)}
+        title="Introduce yourself!"
+      >
+        
+      </Modal>
     </>
   );
 }
