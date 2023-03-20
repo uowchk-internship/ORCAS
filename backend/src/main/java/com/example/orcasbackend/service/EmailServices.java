72a6d1f0d518a1829f6adcb618c1e6dd83f01cee
unclechuen <johnny@johnnyip.com>
2023-03-21 01:11:38 +0800

    Removed mail service

--- backend/src/main/java/com/example/orcasbackend/service/EmailServices.java
+++ backend/src/main/java/com/example/orcasbackend/service/EmailServices.java
@@ -1,35 +1,35 @@
-package com.example.orcasbackend.service;
-
-import com.example.orcasbackend.entity.EmailDetails;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
-
-@Service
-public class EmailServices {
-
-    private JavaMailSender javaMailSender;
-
-    @Autowired
-    public EmailServices(JavaMailSender javaMailSender) {
-        this.javaMailSender = javaMailSender;
-    }
-
-    @Value("${spring.mail.username}")
-    private String sender;
-
-    public String sendEmail(EmailDetails details) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-
-        mailMessage.setFrom(sender);
-        mailMessage.setTo(details.getRecipient());
-        mailMessage.setText(details.getMsgBody());
-        mailMessage.setSubject(details.getSubject());
-
-        javaMailSender.send(mailMessage);
-        return "done";
-    }
-
-}
+//package com.example.orcasbackend.service;
+//
+//import com.example.orcasbackend.entity.EmailDetails;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class EmailServices {
+//
+//    private JavaMailSender javaMailSender;
+//
+//    @Autowired
+//    public EmailServices(JavaMailSender javaMailSender) {
+//        this.javaMailSender = javaMailSender;
+//    }
+//
+//    @Value("${spring.mail.username}")
+//    private String sender;
+//
+//    public String sendEmail(EmailDetails details) {
+//        SimpleMailMessage mailMessage = new SimpleMailMessage();
+//
+//        mailMessage.setFrom(sender);
+//        mailMessage.setTo(details.getRecipient());
+//        mailMessage.setText(details.getMsgBody());
+//        mailMessage.setSubject(details.getSubject());
+//
+//        javaMailSender.send(mailMessage);
+//        return "done";
+//    }
+//
+//}
